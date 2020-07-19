4ca1ec1313da465a113411a52367fbe5309c7c59
hahagu <mingu2001@gmail.com>
2020-07-19 20:35:42 +0900

    🔥 First Page Fix 🔥

--- resources/views/vue.blade.php
+++ resources/views/vue.blade.php
@@ -3,11 +3,12 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Adaptive Website Template">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Laravue</title>
+    <title>Adaptive</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
