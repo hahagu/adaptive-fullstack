dd75ec52a2dbdd51ace4429e70a85ae9cc9cb157
hahagu <mingu2001@gmail.com>
2020-07-28 21:44:39 +0900

    📌 Version 2.2 📌

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
+Current Version: v2.2
 General guides and references moved to the [wiki](https://github.com/hahagu/adaptive-fullstack/wiki).
--- config/app.php
+++ config/app.php
@@ -27,6 +27,18 @@ return [
     */
 
     'env' => env('APP_ENV', 'production'),
+    
+    /*
+    |--------------------------------------------------------------------------
+    | Application Version
+    |--------------------------------------------------------------------------
+    |
+    | This value is the version of your application. This value is used when
+    | the framework needs to place the application's version in a notification
+    | or any other location as required by the application or its packages.
+    */
+
+    'version' => '2.2',
 
     /*
     |--------------------------------------------------------------------------
