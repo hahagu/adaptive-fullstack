9c4c6031911ddc20b3154d8dbb45a45306599cd0
Min Gu Kang <mingu2001@gmail.com>
2021-11-02 12:44:41 +0900

    ⏫ Upgrade Laravel to 8.x ⏫

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,29 +1,34 @@
 <?php
 
-use App\User;
+namespace Database\Seeders;
+
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeders.
      *
      * @return void
      */
     public function run()
     {
-        User::create([
-            'name' => 'Admin',
+        DB::table('users')->insert([
+            'name' => 'Administrator',
             'email' => 'admin@test.com',
             'username' => 'admin',
             'password' => Hash::make('Passw0rd!'),
             'role' => 'admin'
         ]);
-        User::create([
+        DB::table('users')->insert([
             'name' => 'User',
             'email' => 'user@test.com',
             'username' => 'user',
-            'password' => Hash::make('Passw0rd!')
+            'password' => Hash::make('Passw0rd!'),
+            'role' => 'user'
         ]);
     }
 }
